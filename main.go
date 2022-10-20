e472ed1da3ae5cbb19363073b1bd0cd00e7b40a3
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-10-20 11:22:42 +0530

    add titlelist and notecontent

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fyne.io/fyne/v2/app"
+
+func loadUI() fyne.CanvasObject {
+
+	list := widget.NewVBox()
+	toolbar := widget.NewToolbar(
+		widget.NewToolbarAction(theme.ContentAddIcon(), func() {}),
+		widget.NewToolbarAction(theme.ContentRemoveIcon(), func() {}),
+	)
+
+	titlelist := fyne.NewContainerWithLayout(Layout.NewBorderLayout(toolbar, nil, nil, nil), toolbar, list)
+	notecontent := widget.NewMultiLineEntry()
+	return widget.NewHSplitContainer(titlelist, notecontent)
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Notepad")
+	loadUI()
+	w.ShowAndRun()
+}
