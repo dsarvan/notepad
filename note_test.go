98492d575687b9d7a16617abc56b9854c40c8944
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-10-24 14:08:47 +0530

    new instance of note to return content in line 1

--- note_test.go
+++ note_test.go
@@ -9,4 +9,9 @@ import (
 func testNoteTitle(t *testing.T) {
 	tnote := &note{content: "Note Title"}
 	assert.Equal(t, "Note Title", tnote.title())
+
+	// new instance of note to return content in line 1
+	// let first line of the text be called title
+	tnote = &note{content: "Note\nLine2"}
+	assert.Equal(t, "Note", tnote.title())
 }
