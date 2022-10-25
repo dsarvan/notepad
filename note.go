40774131031fba32984c6d9d358fbbc138698137
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-10-25 15:55:19 +0530

    split on new line and return the first line

--- note.go
+++ note.go
@@ -1,10 +1,14 @@
 package main
 
+import "strings"
+
 type note struct {
 	content string
 }
 
 // derive the title from the content
 func (n *note) title() string {
-	return n.content
+	/* split on new line and only have
+	   two lines and return the first line */
+	return strings.SplitN(n.content, "\n", 2)[0]
 }
